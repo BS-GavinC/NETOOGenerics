b66495f737f6d41edec9548cc9b7708501660622
BS-GavinC <98208007+VZGLY@users.noreply.github.com>
2023-05-23 09:54:36 +0200

    Ajoutez des fichiers projet.

---
+++ NETOOGenerics/Program.cs
@@ -0,0 +1,8 @@
+﻿using NETOOGenerics;
+
+DateTime date = DateTime.Now;
+
+User<DateTime> user = new User<DateTime>(date);
+
+Console.WriteLine(user.isMyId(date));
+
---
+++ NETOOGenerics/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETOOGenerics
+{
+    internal class User<Tid>
+    {
+        public User(Tid id)
+        {
+            Id = id;
+        }
+        public Tid Id { get; set; }
+
+        public bool isMyId(Tid id)
+        {
+            return id.Equals(Id);
+        }
+
+    }
+}
